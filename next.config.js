3c760c5806598dae6a31480df380c90e36f63adb
tn75bybjdf-collab <jjasaleh14@aol.com>
2026-07-02 13:59:45 +0300

    security next

--- next.config.js
+++ next.config.js
@@ -2,10 +2,24 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' }
-    ]
-  }
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
